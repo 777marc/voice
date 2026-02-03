aaf3ee056c039f63259e3653047208a02bfd2fe6
Marc Mendoza <777marc@gmail.com>
2026-02-03 11:42:05 -0500

    misc

--- voice.py
+++ voice.py
@@ -3,17 +3,20 @@ import edge_tts
 
 TEXT_SSML = """<speak version='1.0' xml:lang='en-US'>
   <prosody rate='85%' pitch='-2st'>
-    Hello<break time='180ms'/>… nice to meet you.
+    Hello<break time='500ms'/>… nice to meet you.
   </prosody>
 </speak>"""
 
-TEXT_SSML2 = """Hello… nice to meet you."""
+TEXT_SSML2 = """Hello world."""
 
 voice = "en-US-JennyNeural"
 voice2 = "en-US-AvaNeural"
+voice3 = "en-US-AriaNeural"
+pitch = "-25Hz"
+rate = "-20%"
 
 async def main():
-    communicate = edge_tts.Communicate(TEXT_SSML2, voice=voice2)
-    await communicate.save("hello_2.mp3")
+    communicate = edge_tts.Communicate(text=TEXT_SSML2, voice=voice3, pitch=pitch, rate=rate)
+    await communicate.save("hello_t.mp3")
 
 asyncio.run(main())
